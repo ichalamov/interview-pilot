98f8471a52c66c1938c600db375c46e54728d11e
ichalamov <iliya.chalamov@yahoo.com>
2026-04-03 22:25:21 +0300

    update

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Sidebar } from './Sidebar';
-export { default as TopBar } from './TopBar';
-export { default as StepAccordion } from './StepAccordion';
-export { default as GradientButton } from './GradientButton';
-export { default as Card } from './Card';
